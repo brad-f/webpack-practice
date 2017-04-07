54068e92e1532ecb08be2e2e8935831ac288bb8f
brad-f <brad.fitzwater@gmail.com>
2017-04-06 23:23:32 -0400

    added support for devtools

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const CommonsChunkPlugin = require('webpack').optimize.CommonsChunkPlugin;
 const DefinePlugin = require('webpack').DefinePlugin;
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
@@ -5,8 +6,36 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const UglifyJsPlugin = require('webpack').optimize.UglifyJsPlugin;
 
+const production = (value) => (
+  process.env.NODE_ENV === 'production' ? value : undefined
+);
+
+const plugins = () => (
+  _.compact([
+    new CommonsChunkPlugin({
+      name: 'vendor'
+    }),
+    new DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      }
+    }),
+    new ExtractTextPlugin('[name].[hash].css'),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './app/index.html',
+      minify: {
+        collapseWhitespace: true
+      }
+    }),
+    production(
+      new UglifyJsPlugin({ sourceMap: true })
+    )
+  ])
+);
 
 module.exports = {
+  devtool: production('source-map'),
   entry: {
     vendor: ['react', 'react-dom'],
     app: './app/index.js'
@@ -28,23 +57,5 @@ module.exports = {
       })
     }]
   },
-  plugins: [
-    new CommonsChunkPlugin({
-      name: 'vendor'
-    }),
-    new DefinePlugin({
-      'process.env': {
-        'NODE_ENV': '"production"'
-      }
-    }),
-    new ExtractTextPlugin('[name].[hash].css'),
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: './app/index.html',
-      minify: {
-        collapseWhitespace: true
-      }
-    }),
-    new UglifyJsPlugin(),
-  ]
+  plugins: plugins()
 };
